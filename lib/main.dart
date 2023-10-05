aa7d13d21735cc230213f694fea52142f708774d
patgpt4 <channel.42@icloud.com>
2023-10-05 10:07:36 -0600

    🐞 fix:
    bugs!

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, TwitterAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_twitter/firebase_ui_oauth_twitter.dart';
 import 'package:flutter/material.dart';
+import 'package:the_tobacco_club/env.dart';
 import 'package:the_tobacco_club/firebase_options.dart';
 
 void main() async {
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  FirebaseUIAuth.configureProviders([
+    TwitterProvider(
+      apiKey: TWITTER_KEY,
+      apiSecretKey: TWITTER_SECRET,
+    ),
+  ]);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,67 +27,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Tobacco Club',
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 205, 153)),
-        useMaterial3: true,
-      ),
-      // routes: {
-      //   "/": (context) => const MyHomePage(title: 'The Tobacco Club'),
-      //   "/signIn": (context) => const MyHomePage(title: 'Sign in'),
-      // },
-      darkTheme: ThemeData.dark(),
-      home: const MyHomePage(title: 'The Tobacco Club'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _signIn() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to the Tobacco Club app!',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _signIn,
-        tooltip: 'Sign in',
-        child: const Icon(Icons.add_a_photo),
-      ),
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+      routes: {
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+              TwitterProvider(apiKey: TWITTER_KEY, apiSecretKey: TWITTER_SECRET)
+            ],
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }),
+            ],
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            providers: [
+              EmailAuthProvider(),
+              TwitterProvider(apiKey: TWITTER_KEY, apiSecretKey: TWITTER_SECRET)
+            ],
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/sign-in');
+              }),
+            ],
+          );
+        },
+      },
     );
   }
 }
